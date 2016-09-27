e795732d5461f46d581f91f1bd7e7d60761946ea
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-09-27 10:16:10 -0400

    add .create_from_collection and #add_trail_attributes methods to Trail class

--- lib/trail.rb
+++ lib/trail.rb
@@ -13,6 +13,14 @@ class Trail
     Trail.update_state_lists
   end
 
+  def self.create_from_collection(trails_array)
+    trails_array.each {|trail| Trail.new(trail)}
+  end
+
+  def add_trail_attributes(attributes_hash)
+    attributes_hash.each {|key, value| self.send("#{key}=", "#{value}")}
+  end
+
   def self.update_state_lists
     self.all.each do |trail|
       case trail.state
@@ -28,7 +36,6 @@ class Trail
         self.wv << trail
       end
     end
-    binding.pry
   end
 
   def self.all
